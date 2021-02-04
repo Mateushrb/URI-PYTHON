ce4409e153a0f7a9099fff8e4830aac15f3ca112
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-02-04 20:18:55 -0300

    Add files via upload

---
+++ Iniciante/1046 - Tempo de Jogo.py
@@ -0,0 +1,9 @@
+# Entrada de dados
+h_inicial, h_final = [int(x) for x in input().split()]
+
+# Cálculo
+if h_inicial < h_final:
+    duracao = h_final - h_inicial
+else:
+    duracao = 24 - h_inicial + h_final
+print("O JOGO DUROU %i HORA(S)" %duracao)
