dd625b478c2f5c597415e54e8dc04a725360a8bf
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-02-03 21:47:07 -0300

    Add files via upload

---
+++ Iniciante/1041 - Coordenadas de um Ponto.py
@@ -0,0 +1,18 @@
+# Entrada
+x, y = [float(x) for x in input().split()]
+
+# Seleção
+if x==0 and y==0:
+    print("Origem")
+elif x==0 and (y>0 or y<0):
+    print("Eixo Y")
+elif y==0 and (y>0 or x<0):
+    print("Eixo X")
+elif x>0 and y>0:
+    print("Q1")
+elif x<0 and y>0:
+    print("Q2")
+elif x<0 and y<0:
+    print("Q3")
+else:
+    print("Q4")
