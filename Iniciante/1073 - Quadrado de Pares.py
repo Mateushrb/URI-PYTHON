3fe686c1f174e6c106f46b62b42f1c0858d0a5ad
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-02-08 18:19:17 -0300

    Add files via upload

---
+++ Iniciante/1073 - Quadrado de Pares.py
@@ -0,0 +1,9 @@
+# Valor de entrada
+n = int(input())
+
+valor = 2
+# Estrutura de repetição com saída de dados
+while valor <= n:
+    quadrado = valor**2
+    print("%i^2 = %i" %(valor, quadrado))
+    valor += 2
