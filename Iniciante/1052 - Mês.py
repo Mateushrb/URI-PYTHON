27c3b8e9fc2b26567330fb7ecda05ec0a20aa9d2
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-02-08 13:32:23 -0300

    Add files via upload

---
+++ Iniciante/1052 - Mês.py
@@ -0,0 +1,8 @@
+# Valor de entrada
+mes = int(input())
+
+# Lista
+meses = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+# Saída de dados
+print(meses[mes-1])
