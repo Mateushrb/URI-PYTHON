458bb0bb4011c9eae9a61ef57947cb6dabc8b27c
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-02-08 17:26:48 -0300

    Add files via upload

---
+++ Iniciante/1070 - Seis Números Ímpares.py
@@ -0,0 +1,11 @@
+# Entrada
+x = int(input())
+
+# Condição para número ímpar
+if x%2 == 0:
+    x += 1
+
+# Estrutura de repetição com saída de dados
+for i in range(0, 6, 1):
+    print(x)
+    x += 2
