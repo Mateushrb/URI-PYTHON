e3f89a80c0bb4ea06cc736476fb4d9414d122513
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-02-08 22:05:10 -0300

    Add files via upload

---
+++ Iniciante/1097 - Sequencia IJ 3.py
@@ -0,0 +1,11 @@
+# Variáveis necessárias
+I = 1
+J = 7
+
+# Estruturas de repetição com saida
+for i in range(0, 5, 1):
+    for l in range(0, 3, 1):
+        print("I=%i J=%i" %(I, J))
+        J -= 1
+    I += 2
+    J += 5
