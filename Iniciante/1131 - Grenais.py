0b49359fee529bc046572135059f07c4d19e1c93
Mateus Henrique <73316062+Mateushrb@users.noreply.github.com>
2021-03-31 11:51:28 -0300

    Update 1131 - Grenais.py

--- Iniciante/1131 - Grenais.py
+++ Iniciante/1131 - Grenais.py
@@ -1,28 +1,39 @@
+# Variável de condição para novos Grenais #
 rep = 1
 
+### Variáveis para elaboração da estatistica ###
 grenais = 0
 inter = 0
 empates = 0
 gremio = 0
+###
 
-while rep == 1:
+while rep == 1: # Estrutura de repetição dos grenais
+    
+    # Quantidade de gols
     gols_inter, gols_gremio = [int(x) for x in input().split()]
+    
+    # Quantidade de Grenais
     grenais += 1
-    if gols_inter > gols_gremio:
+    
+    # Condições
+    if gols_inter > gols_gremio: # Condição para vitória do Inter
         inter += 1
-    elif gols_inter == gols_gremio:
+    elif gols_inter == gols_gremio: # Condição para empate
         empates += 1
-    else:
+    else: # Condição para vitória do gremio
         gremio += 1
     
-    print("Novo grenal (1-sim 2-nao)")
-    rep = int(input())
+    print("Novo grenal (1-sim 2-nao)") # Pergunta para novo Grenal
+    rep = int(input()) # Resposta da pergunta
 
+# Impressão dos resultados
 print("%s grenais" %(grenais))
 print("Inter:%s" %(inter))
 print("Gremio:%s" %(gremio))
 print("Empates:%s" %(empates))
 
+# Impressão do vencedor ou empate
 if inter>gremio:
     print("Inter venceu mais")
 elif inter == gremio:
